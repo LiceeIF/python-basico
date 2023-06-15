2037ca9d3ce8022c3f0ac83a54c4c48069ff5590
LiceeIF <a.alicemaria.oliveira@gmail.com>
2023-06-14 22:54:54 -0300

    iniciando o repositório

---
+++ README.md
@@ -0,0 +1,4 @@
+# PYTHON BÁSICO 
+## Introdução à linguagem de programação python
+### conceitos iniciais e pequenos projetos
+
---
+++ hello.py
@@ -0,0 +1,22 @@
+print("Meu primeiro esboço")
+
+#atribuindo valor a string: 
+nome = 'Alice'
+segundoNome = 'Maria'
+nome_completo = nome + ' ' + segundoNome
+print('o nome fornecido foi ' + nome_completo)
+
+nome_1 = 'Fernanda'
+nome_2 = 'Fernanda'
+
+if nome_1 == nome_2:
+    print('Nomes fornecidos são iguais')
+else:
+    print('Nomes fornecidos são diferentes')
+
+#lendo valores fornecidos pelo usuário
+cor = input("Qual é a sua cor favorita? ")
+print("então sua cor favorita é: " + cor)
+
+
+
